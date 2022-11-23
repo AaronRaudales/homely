1b5f890687345f3fa898e4cbb381b134f1917112
Aaron Raudales <78047697+AaronRaudales@users.noreply.github.com>
2022-11-23 01:28:45 -0600

    Update urls.ts

--- src/app/theme/shared/services/urls.ts
+++ src/app/theme/shared/services/urls.ts
@@ -1,5 +1,5 @@
 export const urls = {
-    urlLogin: "login",
+    urlLogin: "https://homely-back.herokuapp.com/login",
     urlForgotPassword: "forgot-password",
     urlSignUp: "signup",
     urlPropiedadesPost:"api/propiedades/add",
